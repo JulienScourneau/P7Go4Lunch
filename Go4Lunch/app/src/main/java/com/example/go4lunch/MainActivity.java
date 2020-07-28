81a48748f16864278a3b5d9733ed10517989b258
JulienScourneau <56632291+JulienScourneau@users.noreply.github.com>
2020-07-28 19:12:07 +0200

    Add FirebaseUi signInActivity

--- Go4Lunch/app/src/main/java/com/example/go4lunch/MainActivity.java
+++ Go4Lunch/app/src/main/java/com/example/go4lunch/MainActivity.java
@@ -4,12 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.firebase.ui.auth.AuthUI;
+
+import java.util.Arrays;
+
+import jp.wasabeef.blurry.Blurry;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int a;
+
+        signInActivity();
+
+    }
+
+    private void signInActivity(){
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setTheme(R.style.LoginTheme)
+                        .setLogo(R.drawable.icon1)
+                        .setAvailableProviders(Arrays.asList(
+                                new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build(),
+                                new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
+                        .setIsSmartLockEnabled(false, true)
+                        .build(),
+                RC_SIGN_IN);
     }
 }
