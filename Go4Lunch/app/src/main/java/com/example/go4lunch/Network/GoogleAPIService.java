ce12d609b044ac15b06eae19018db8adb65d5a75
JulienScourneau <56632291+JulienScourneau@users.noreply.github.com>
2020-09-28 14:52:57 +0200

    Update GoogleAPIService.java

--- Go4Lunch/app/src/main/java/com/example/go4lunch/Network/GoogleAPIService.java
+++ Go4Lunch/app/src/main/java/com/example/go4lunch/Network/GoogleAPIService.java
@@ -1,4 +1,11 @@
 package com.example.go4lunch.Network;
 
+import com.example.go4lunch.Models.MyPlaces;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface GoogleAPIService {
+    @GET
+    Call<MyPlaces> getPlaces();
 }
