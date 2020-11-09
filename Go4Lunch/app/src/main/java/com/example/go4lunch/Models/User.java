6d1ec24bb2774783f3d97f9314e914246d085e7d
JulienScourneau <56632291+JulienScourneau@users.noreply.github.com>
2020-11-09 13:46:53 +0100

    Update User class for FireStore

--- Go4Lunch/app/src/main/java/com/example/go4lunch/Models/User.java
+++ Go4Lunch/app/src/main/java/com/example/go4lunch/Models/User.java
@@ -2,27 +2,47 @@ package com.example.go4lunch.Models;
 
 public class User {
 
-    private int mUserAvatar;
+    private String mUid;
+    private String mUserName;
     private String mUserStatus;
+    private int mUserAvatar;
 
-    public User(int userAvatar, String userStatus){
+    public User(String uid, String userName, String userStatus, int userAvatar) {
+        this.mUid = uid;
+        this.mUserName = userName;
         this.mUserAvatar = userAvatar;
         this.mUserStatus = userStatus;
     }
 
-    public int getUserAvatar() {
-        return mUserAvatar;
+    public String getUid() {
+        return mUid;
+    }
+
+    public String getUserName() {
+        return mUserName;
     }
 
     public String getUserStatus() {
         return mUserStatus;
     }
 
-    public void setUserAvatar(int mUserAvatar) {
-        this.mUserAvatar = mUserAvatar;
+    public int getUserAvatar() {
+        return mUserAvatar;
+    }
+
+    public void setUid(String mUid) {
+        this.mUid = mUid;
+    }
+
+    public void setUserName(String mUserName) {
+        this.mUserName = mUserName;
     }
 
     public void setUserStatus(String mUserStatus) {
         this.mUserStatus = mUserStatus;
+    }
+
+    public void setUserAvatar(int mUserAvatar) {
+        this.mUserAvatar = mUserAvatar;
     }
 }
