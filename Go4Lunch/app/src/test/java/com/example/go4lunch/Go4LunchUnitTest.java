75741f2da659b9e9e04a74eeba1831205431b589
JulienScourneau <56632291+JulienScourneau@users.noreply.github.com>
2021-03-08 16:02:36 +0100

    Add last

--- Go4Lunch/app/src/test/java/com/example/go4lunch/Go4LunchUnitTest.java
+++ Go4Lunch/app/src/test/java/com/example/go4lunch/Go4LunchUnitTest.java
@@ -1,14 +1,21 @@
 package com.example.go4lunch;
 
-import android.telecom.Call;
-
 import com.example.go4lunch.Models.Details.PlaceDetails;
+import com.example.go4lunch.Models.Details.Result;
+import com.example.go4lunch.Models.NearbySearch.Geometry;
+import com.example.go4lunch.Models.NearbySearch.Locations;
+import com.example.go4lunch.Models.NearbySearch.OpeningHours;
+import com.example.go4lunch.Models.NearbySearch.Photo;
 import com.example.go4lunch.Utils.Utils;
 import com.google.android.gms.maps.model.LatLng;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -19,6 +26,41 @@ import static org.junit.Assert.*;
  */
 @RunWith(JUnit4.class)
 public class Go4LunchUnitTest {
+
+    private PlaceDetails placeDetails = new PlaceDetails();
+    private Result detailsResult = new Result();
+    private Double lat;
+    private Double lng;
+    private int radius;
+    private String fakeId;
+
+    @Before
+    public void setup() {
+        lat = 1.00;
+        lng = 1.00;
+        radius = 0;
+        fakeId = "fakeId";
+        detailsResult.setPlaceId("fakePlaceId");
+        detailsResult.setName("fakeName");
+        List<Photo> list = new ArrayList<>();
+        Photo photo = new Photo();
+        photo.setPhotoReference("https://pbs.twimg.com/profile_images/1363824536618229760/wr1Z_VKN_normal.jpg");
+        list.add(photo);
+        detailsResult.setPhotos(list);
+        detailsResult.setVicinity("fakeVicinity");
+        Geometry geometry = new Geometry();
+        Locations locations = new Locations();
+        locations.setLat(lat);
+        locations.setLng(lng);
+        geometry.setLocations(locations);
+        detailsResult.setGeometry(geometry);
+        detailsResult.setRating(0.00);
+        OpeningHours openingHours = new OpeningHours();
+        openingHours.setOpenNow(true);
+        detailsResult.setOpeningHours(openingHours);
+        placeDetails.setResult(detailsResult);
+    }
+
     @Test
     public void change_the_ranking() {
         float rating = Utils.getRating(2.00);
@@ -27,22 +69,27 @@ public class Go4LunchUnitTest {
 
     @Test
     public void get_details_place_url() {
-        String fakeId = "fakeId";
         String url = Utils.getPlaceDetailsUrl(fakeId);
-        assertEquals( "details/json?place_id=fakeId&key=AIzaSyD6y_8l1WeKKDk0dOHxxgL_ybA4Lmjc1Cc",url);
+        assertEquals("details/json?place_id=fakeId&key=AIzaSyD6y_8l1WeKKDk0dOHxxgL_ybA4Lmjc1Cc", url);
     }
 
     @Test
     public void get_nearby_place_url() {
-        LatLng latLng = new LatLng(0.00,0.00) ;
-        int radius = 0;
+        LatLng latLng = new LatLng(lat, lng);
         String url = Utils.getNearbyPlaceUrl(latLng, radius);
-        assertEquals("nearbysearch/json?location=0.0,0.0&radius=0&types=restaurant&sensor=true&key=AIzaSyD6y_8l1WeKKDk0dOHxxgL_ybA4Lmjc1Cc",url);
+        assertEquals("nearbysearch/json?location=1.0,1.0&radius=0&types=restaurant&sensor=true&key=AIzaSyD6y_8l1WeKKDk0dOHxxgL_ybA4Lmjc1Cc", url);
     }
 
     @Test
     public void change_details_on_nearby_place() {
-        PlaceDetails placeDetails = new PlaceDetails();
-        Utils.changeDetailsResult(placeDetails);
+        com.example.go4lunch.Models.NearbySearch.Result nearbyResult = Utils.changeDetailsResult(placeDetails);
+
+        assertEquals(detailsResult.getPlaceId(), nearbyResult.getPlaceId());
+        assertEquals(detailsResult.getName(), nearbyResult.getName());
+        assertEquals(detailsResult.getPhotos().get(0), nearbyResult.getPhotos().get(0));
+        assertEquals(detailsResult.getVicinity(), nearbyResult.getVicinity());
+        assertEquals(detailsResult.getGeometry(), nearbyResult.getGeometry());
+        assertEquals(detailsResult.getRating(), nearbyResult.getRating());
+        assertTrue(nearbyResult.getOpeningHours().getOpenNow());
     }
 }
